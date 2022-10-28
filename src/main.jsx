db7d3ed8b5a1cfe3366b1a8197696ce578305d1d
quanscheng <quanscheng@163.com>
2022-10-28 17:04:41 +0800

    feat: 子路由错误拦截,UX

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import ErrorPage from './error-page'
 import './index.css'
 import Index from './routes'
 import Contact, {
-  action as contactAction, loader as contactLoader
+  action as contactAction,
+  loader as contactLoader
 } from './routes/contact'
 import { action as destroyAction } from './routes/destroy'
 import EditContact, { action as editAction } from './routes/edit'
@@ -20,32 +21,35 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        index: true,
-        element: <Index />,
-      },
-      {
-        path: 'contacts/:contactId',
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
-      },
-      {
-        path: 'contacts/:contactId/edit',
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: 'contacts/:contactId/destroy',
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: 'contacts/:contactId',
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: 'contacts/:contactId/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: 'contacts/:contactId/destroy',
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
       },
     ],
   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )
