d0bd94ceaac02ceb5b6510149aa9962e459cee1b
quanscheng <quanscheng@163.com>
2022-10-28 11:49:03 +0800

    feat: 创建路由和根组件

---
+++ readme.md
@@ -0,0 +1,32 @@
+# react-router-6.4+ tutorial
+
+## intall dependencies
+
+```
+npm install react-router-dom localforage match-sorter sort-by
+```
+
+## run
+
+```
+npm run dev
+```
+
+## prepare styles
+
+`src/index.css`, `src/contacts.js`
+
+## Delete unused files
+
+```
+src
+├── contacts.js
+├── index.css
+└── main.jsx
+```
+
+## add router
+
+`main.js`
+
+## 创建`src/routes`和`src/routes/root.jsx`
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import Root from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
